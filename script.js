2cfd628ac4e5763faa3ea0d9bdb3dacb3e40b013
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 20:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
-  // your code here
+	str=""
+  for (let i=0;i<s.length;i++){
+if(str[i]===" "){
+	return str
+}
+	  str+=s[i]
+	  
+  }
+	return str
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
